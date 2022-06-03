b004d9947354079d5d544737f1c104a301cbc784
janikvonrotz <contact@janikvonrotz.ch>
2022-06-03 12:32:26 +0200

    [forestry_timesheet] update

--- forestry_timesheet/models/account_analytic_line.py
+++ forestry_timesheet/models/account_analytic_line.py
@@ -4,8 +4,27 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = "account.analytic.line"
+    _inherit = 'account.analytic.line'
 
     product_id = fields.Many2one('product.product', check_company=True)
+    category_id = fields.Many2one('product.category')
+    product_qty = fields.Float(string='Quantity')
+    product_stock_uom_category_id = fields.Many2one('uom.category', related='product_id.uom_id.category_id')
+    product_stock_uom_id = fields.Many2one('uom.uom', string='Unit of Measure', domain="[('category_id', '=', product_stock_uom_category_id)]")
     location_id = fields.Many2one('stock.location', 'Source Location', check_company=True)
     location_dest_id = fields.Many2one('stock.location', 'Destination Location', check_company=True)
+    trips = fields.Integer()
+
+    @api.onchange('product_id')
+    def onchange_product(self):
+        self.category_id = self.product_id.categ_id
+        self.product_stock_uom_id = self.product_id.uom_id
+        quant = self.env['stock.quant'].search([('product_id', '=', self.product_id.id)], limit=1)
+        if quant:
+            self.location_id = quant.location_id
+
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     lines = super(AccountAnalyticLine, self).create(vals_list)
+    #     _logger.warning([lines,vals_list])
+    #     return lines
---
+++ forestry_timesheet/wizard/project_task_create_timesheet.py
@@ -0,0 +1,46 @@
+from odoo import fields, models, api
+import logging
+_logger = logging.getLogger(__name__)
+from datetime import datetime
+
+
+class ProjectTaskCreateTimesheet(models.TransientModel):
+    _inherit = 'project.task.create.timesheet'
+
+    product_id = fields.Many2one('product.product', check_company=True)
+    category_id = fields.Many2one('product.category')
+    product_qty = fields.Float(string='Quantity')
+    product_stock_uom_category_id = fields.Many2one('uom.category', related='product_id.uom_id.category_id')
+    product_stock_uom_id = fields.Many2one('uom.uom', string='Unit of Measure', domain="[('category_id', '=', product_stock_uom_category_id)]")
+    company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, default=lambda self: self.env.company)
+    location_id = fields.Many2one('stock.location', 'Source Location', check_company=True)
+    location_dest_id = fields.Many2one('stock.location', 'Destination Location', check_company=True)
+    trips = fields.Integer()
+
+    @api.onchange('product_id')
+    def onchange_product(self):
+        self.category_id = self.product_id.categ_id
+        self.product_stock_uom_id = self.product_id.uom_id
+        quant = self.env['stock.quant'].search([('product_id', '=', self.product_id.id)], limit=1)
+        if quant:
+            self.location_id = quant.location_id
+
+    def save_timesheet(self):
+        # res = super().save_timesheet()
+        values = {
+            'task_id': self.task_id.id,
+            'project_id': self.task_id.project_id.id,
+            'date': fields.Date.context_today(self),
+            'name': self.description,
+            'user_id': self.env.uid,
+            'unit_amount': self.task_id._get_rounded_hours(self.time_spent * 60),
+            'product_id': self.product_id.id,
+            'category_id': self.category_id.id,
+            'product_qty': self.product_qty,
+            'product_stock_uom_id': self.product_stock_uom_id.id,
+            'location_id': self.location_id.id,
+            'location_dest_id': self.location_dest_id.id,
+            'trips': self.trips,
+        }
+        self.task_id.user_timer_id.unlink()
+        return self.env['account.analytic.line'].create(values)
