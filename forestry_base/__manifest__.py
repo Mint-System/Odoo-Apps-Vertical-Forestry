d45d4ef3aa95fe5d81ca0393489a472e661a3270
janikvonrotz <contact@janikvonrotz.ch>
2022-06-03 12:33:06 +0200

    bump version

--- forestry_base/__manifest__.py
+++ forestry_base/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Forestry",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "license": "OPL-1",
     "depends": ["contacts", "project", "stock", "hr_fleet", "hr_timesheet", "project_enterprise"],
     "installable": True,
