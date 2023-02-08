cafd30510ee2f2ef5e7b2b7963b1edc5626d4be8
janikvonrotz <contact@janikvonrotz.ch>
2023-02-08 11:50:23 +0100

    [forestry_timesheet] udpate translations

--- forestry_timesheet/models/account_analytic_line.py
+++ forestry_timesheet/models/account_analytic_line.py
@@ -17,9 +17,6 @@ class AccountAnalyticLine(models.Model):
     carrier_id = fields.Many2one('res.partner', 'Carrier', check_company=True)
     trips = fields.Integer()
 
-    # stock_location_id = fields.Many2one('stock.location', 'Source Location', check_company=True)
-    # stock_location_dest_id = fields.Many2one('stock.location', 'Destination Location', check_company=True)
-
     @api.onchange('product_id')
     def onchange_product(self):
         """Set defaults product is selected."""
@@ -27,14 +24,3 @@ class AccountAnalyticLine(models.Model):
         self.product_stock_uom_id = self.product_id.uom_id
         self.location_id = self.task_id.location_id
         self.location_dest_id = self.task_id.location_dest_id
-
-        # quant = self.env['stock.quant'].search([('product_id', '=', self.product_id.id)], limit=1)
-        # if quant:
-        #     self.location_id = quant.location_id
-        # self.location_dest_id = self.task_id.partner_id.property_stock_customer
-
-    # @api.model_create_multi
-    # def create(self, vals_list):
-    #     lines = super(AccountAnalyticLine, self).create(vals_list)
-    #     _logger.warning([lines,vals_list])
-    #     return lines
