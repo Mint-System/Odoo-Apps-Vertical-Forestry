973cd548feeb8df2666a658fd0bd8174f0dc5095
Janik von Rotz <login@janikvonrotz.ch>
2023-11-23 09:07:05 +0100

    update forestry project, stock and timesheet

--- forestry_stock/__manifest__.py
+++ forestry_stock/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Administration",
-    "version": "15.0.4.0.0",
+    "version": "15.0.4.1.0",
     "license": "OPL-1",
     "depends": ["forestry_timesheet"],
     "data": ["views/hr_timesheet.xml", "views/stock.xml"],
--- forestry_stock/models/account_analytic_line.py
+++ forestry_stock/models/account_analytic_line.py
@@ -9,10 +9,10 @@ from odoo.exceptions import UserError
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    def _action_apply_inventory(
-        self, product_id, product_stock_uom_id, product_qty, operation="remove"
+    def _action_remove_inventory(
+        self, product_id, product_stock_uom_id, product_qty
     ):
-        """Update on hand qty."""
+        """Remove on hand qty."""
 
         self.ensure_one()
 
@@ -33,54 +33,67 @@ class AccountAnalyticLine(models.Model):
             )
 
             # Remove quantity
-            if operation == "remove":
-                if (quant.quantity - line_quantity) < 0.0:
-                    raise UserError(
-                        _(
-                            "Cannot apply inventory change for %s as result would be negative."
-                        )
-                        % product_id.display_name
+            if (quant.quantity - line_quantity) < 0.0:
+                raise UserError(
+                    _(
+                        "Cannot apply inventory change for %s as result would be negative."
                     )
-                quant.inventory_quantity = quant.quantity - line_quantity
-                quant.action_apply_inventory()
+                    % product_id.display_name
+                )
+            quant.inventory_quantity = quant.quantity - line_quantity
+            quant.action_apply_inventory()
 
-            # Add quantity
-            if operation == "add":
-                if not quant:
-                    quant = self.env["stock.quant"].create(
-                        {
-                            "product_id": product_id.id,
-                            "location_id": self.env.ref(
-                                "stock.stock_location_stock"
-                            ).id,
-                        }
-                    )
+    def _action_increase_inventory(
+        self, product_id, product_stock_uom_id, product_qty
+    ):
+        """Increase on hand qty."""
 
-                if quant.quantity < 0.0:
-                    raise UserError(
-                        _(
-                            "No empty or positive inventory has been found for product %s."
-                        )
-                        % product_id.display_name
-                    )
-                quant.inventory_quantity = quant.quantity + line_quantity
-                quant.action_apply_inventory()
+        self.ensure_one()
+
+        # Check if line has storable product and is linked to a task
+        if self.task_id and product_id.type == "product":
+
+            # Get quant of product
+            quant = self.env["stock.quant"].search(
+                [
+                    ("product_id", "=", product_id.id),
+                ],
+                limit=1,
+            )
+
+            # Convert uom
+            line_quantity = product_stock_uom_id._compute_quantity(
+                product_qty, product_id.uom_id, rounding_method="HALF-UP"
+            )
+
+            # Increase quantity
+            if not quant:
+                quant = self.env["stock.quant"].create(
+                    {
+                        "product_id": product_id.id,
+                        "location_id": self.env.ref(
+                            "stock.stock_location_stock"
+                        ).id,
+                    }
+                )
+
+            quant.inventory_quantity = quant.quantity + line_quantity
+            quant.action_apply_inventory()
 
     def action_validate_timesheet(self):
         """Set product qty if timesheet entry is validated."""
         for line in self:
             if line.product_id:
-                line._action_apply_inventory(
+                line._action_remove_inventory(
                     product_id=line.product_id,
                     product_stock_uom_id=line.product_stock_uom_id,
                     product_qty=line.product_qty,
                 )
             if line.product_dest_id:
-                line._action_apply_inventory(
+                line._action_increase_inventory(
                     product_id=line.product_dest_id,
                     product_stock_uom_id=line.product_dest_stock_uom_id,
                     product_qty=line.product_dest_qty,
-                    operation="add",
                 )
         return super().action_validate_timesheet()
 
@@ -89,17 +102,15 @@ class AccountAnalyticLine(models.Model):
         res = super().action_invalidate_timesheet()
         for line in self:
             if line.product_id:
-                line._action_apply_inventory(
+                line._action_increase_inventory(
                     product_id=line.product_id,
                     product_stock_uom_id=line.product_stock_uom_id,
                     product_qty=line.product_qty,
-                    operation="add",
                 )
             if line.product_dest_id:
-                line._action_apply_inventory(
+                line._action_remove_inventory(
                     product_id=line.product_dest_id,
                     product_stock_uom_id=line.product_dest_stock_uom_id,
                     product_qty=line.product_dest_qty,
                 )
-        # self.write({'validated': False})
         return res
